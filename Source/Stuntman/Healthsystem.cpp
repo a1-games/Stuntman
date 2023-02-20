1530dffe18e6af729e925204f77240067f0b34ef
Ribbo <myworkspace22@gmail.com>
2023-02-20 12:58:33 +0100

    prut

---
+++ Source/Stuntman/Healthsystem.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Healthsystem.h"
+
+Healthsystem::Healthsystem()
+{
+}
+
+Healthsystem::~Healthsystem()
+{
+}
